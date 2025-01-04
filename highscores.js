ffc35509efb28023c11fbf3f166e59ab1faa8416
3 <hakimaachak31@gmail.com>
2025-01-04 02:31:19 +0100

    test highscores.js

--- highscores.js
+++ highscores.js
@@ -1,7 +1,8 @@
 const highScoresList = document.querySelector('#highScoresList')
 const highScores = JSON.parse(localStorage.getItem("highScores")) || []
 
+// Afficher les scores dans un élément HTML
 highScoresList.innerHTML =
 highScores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
+    return `<li class="high-score">${score.name} - ${score.score} points</li>`
 }).join("")
